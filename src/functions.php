cb0a24e19faef812204d607536b3474509b857ee
Simon Asika <asika32764@gmail.com>
2026-05-13 19:24:36 +0800

    Fix wait

--- src/functions.php
+++ src/functions.php
@@ -3,10 +3,19 @@
 declare(strict_types=1);
 
 namespace Lyrasoft\Toolkit {
-    if (function_exists('\Lyrasoft\Toolkit\wait_util_truthy')) {
-        function wait_util_truthy(\Closure $callback, int|float $wait = 1000, ?int $maxTimes = null): mixed
+    if (!function_exists('\Lyrasoft\Toolkit\wait_until_truthy')) {
+        /**
+         * Wait until the callback returns a truthy value.
+         *
+         * @param \Closure        $callback
+         * @param int|float       $seconds  Sleep time between tries in seconds (supports decimals, default 1.0 s)
+         * @param int|float|null  $timeout  Maximum total wait time in milliseconds. If null, will wait indefinitely.
+         *
+         * @return mixed
+         */
+        function wait_until_truthy(\Closure $callback, int|float $seconds = 1.0, int|float|null $timeout = null): mixed
         {
-            $i = 0;
+            $start = microtime(true);
 
             while (true) {
                 $value = $callback();
@@ -15,14 +24,16 @@ namespace Lyrasoft\Toolkit {
                     return $value;
                 }
 
-                $i++;
-
-                if ($maxTimes !== null && $i >= $maxTimes) {
-                    return null;
+                // If timeout provided, check elapsed time (in milliseconds)
+                if ($timeout !== null) {
+                    $elapsed = (microtime(true) - $start) * 1000;
+                    if ($elapsed >= $timeout) {
+                        return null;
+                    }
                 }
 
-                // usleep
-                usleep((int) ($wait * 1000));
+                // usleep expects microseconds; $seconds may be fractional
+                usleep((int) ($seconds * 1000000));
             }
         }
     }
